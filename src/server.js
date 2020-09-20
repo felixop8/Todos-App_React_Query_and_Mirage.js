a7d0ca20361d0a2560f6cc603233e31a4ba1ce41
Felix Lopez <felix.lopez@vacasa.com>
2020-09-20 12:34:23 -0700

    Refactor code to use mirage shorthands.

--- src/server.js
+++ src/server.js
@@ -1,120 +1,70 @@
-import { Server, Response } from "miragejs";
-import { v4 } from 'uuid';
+import { Server, JSONAPISerializer,  Model, Factory } from "miragejs";
 
 
 export function makeServer({ environment = "test" } = {}) {
     let server = new Server({
       environment,
+
+      models: {
+        todos: Model,
+      },
+
+      factories: {
+        todo: Factory.extend({
+          text(i) {
+            return `Todo text ${i}` // Todo text 1, Todo text 2, ...
+          },
+          completed: false,
+        }),
+      },
+
+      seeds(server) {
+        server.create("todo")
+        server.create("todo")
+        server.create("todo", { completed: true })
+      },
+
+      serializers: {
+        application: JSONAPISerializer,
+      },
   
       routes() {
         this.namespace = "api";
-        this.timing = 2000
-        const failureRate = 0.5;
+        // this.timing = 2000
 
-        const fakeDatabase = {
-            todos: [{
-              id: "1234",
-              text: 'hey',
-              completed: true,
-            }, {
-              id: v4(),
-              text: 'ho',
-              completed: true,
-            }, {
-              id: v4(),
-              text: 'let’s go',
-              completed: false,
-            }],
-          };
+        // Get todos.
+        this.get("/todos")
 
+        // Create todo.
+        this.post("/todos");
+
+        // Get todo by id.
+        this.get("/todos/:id");
+
+        // Edit todo by id.
+        this.patch("/todos/:id")
+
+        // Delete todo by id.
+        this.del("/todos/:id")
+
+        // Get todos with pagination.
+        this.get("/todos/pagination", (schema, request) => {
+          const {queryParams: { pageOffset, pageSize }} = request
+
+          const todos = schema.db.todos;
+      
+          if (Number(pageSize)) {
+              const start = Number(pageSize) * Number(pageOffset)
+              const end = start + Number(pageSize)
+              const page = todos.slice(start, end)
           
-        // Fetch all todos.
-        this.get("/todos", (schema, request) => {
-            const {queryParams: { pageOffset, pageSize }} = request
-        
-            const todos = fakeDatabase.todos;
-        
-            if (Number(pageSize)) {
-                const start = Number(pageSize) * Number(pageOffset)
-                const end = start + Number(pageSize)
-                const page = todos.slice(start, end)
-            
-                return {
-                    items: page,
-                    nextPage: todos.length > end ? Number(pageOffset) + 1 : undefined,
-                }
-            }
-            return todos;
-        });
-
-
-        // Create todo
-        this.post("/todos", (schema, request) => {
-            if (Math.random() > failureRate) return new Response(400, { some: 'header' }, { errors: [ 'An unknown error occurred!'] });
-
-            const todo = {
-              id: v4(),
-              ...JSON.parse(request.requestBody),
-              completed: false,
-            };
-
-            fakeDatabase.todos.push(todo);
-
-            return todo;
-        });
-
-
-        // Get single todo
-        this.get("/todos/:todoId", (schema, request) => {
-          const {params: {todoId}} = request;
-        
-          const todo = fakeDatabase.todos.find((todo) => todo.id === todoId)
-        
-          if (!todo) return new Response(400, { some: 'header' }, { errors: [ 'Not found.'] });
-
-          return todo;
-        });
-
-
-        // Edit/save todo
-        this.patch("/todos/:todoId", (schema, request) => {
-          if (Math.random() > failureRate) return new Response(400, { some: 'header' }, { errors: [ 'An unknown error occurred!'] });
-
-          const {params: {todoId}, requestBody } = request;
-
-          const todo = fakeDatabase.todos.find(todo => todo.id === todoId);
-
-          if (!todo) new Response(400, { some: 'header' }, { errors: [ 'Not found.'] });
-
-
-          const requestBodyJSObject = JSON.parse(requestBody)
-          delete requestBodyJSObject.id
-
-          const newTodo = {
-            ...todo,
-            ...requestBodyJSObject
+              return {
+                  items: page,
+                  nextPage: todos.length > end ? Number(pageOffset) + 1 : undefined,
+              }
           }
-
-          fakeDatabase.todos = fakeDatabase.todos.map((todo) => (todo.id === todoId ? newTodo : todo))
-
-          return newTodo;
-        });
-
-
-        // Delete single todo
-        this.delete("/todos/:todoId", (schema, request) => {
-          const {params: {todoId} } = request;
-
-          if (Math.random() > failureRate) return new Response(400, { some: 'header' }, { errors: [ 'An unknown error occurred!'] });
-        
-          const todo = fakeDatabase.todos.find((todo) => todo.id === todoId)
-        
-          if (!todo) return new Response(400, { some: 'header' }, { errors: [ 'Not found.'] });
-
-          fakeDatabase.todos = fakeDatabase.todos.filter((todo) => todo.id !== todoId)
-
-          return 'Resource Deleted';
-        });
+          return todos
+      });
       },
     })
   
