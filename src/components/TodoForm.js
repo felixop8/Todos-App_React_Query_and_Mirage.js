99ade4057ecf30f5605d14be8ca6ca5aff5e695c
Felix Lopez <felix.lopez@vacasa.com>
2020-09-21 15:39:48 -0700

    Adds todo form to create new todos.

---
+++ src/components/TodoForm.js
@@ -0,0 +1,53 @@
+import React from 'react'
+
+const defaultFormValues ={
+    title: '',
+    description: ''
+}
+
+export default function TodoForm({initialValues = defaultFormValues, onSubmit, submitText}) {
+
+    const [values, setValues] = React.useState(initialValues)
+
+    const setValue = (field, value) =>
+      setValues((old) => ({ ...old, [field]: value }))
+  
+    const handleSubmit = (e) => {
+      setValues(defaultFormValues)
+      e.preventDefault()
+
+      onSubmit({data: {attributes: {...values, completed: false}, type: 'todos'}})
+    }
+  
+    React.useEffect(() => {
+      setValues(initialValues)
+    }, [initialValues])
+  
+    return (
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="title">Title</label>
+        <div>
+          <input
+            type="text"
+            name="title"
+            value={values.title}
+            onChange={(e) => setValue('title', e.target.value)}
+            required
+          />
+        </div>
+        <br />
+        <label htmlFor="Description">Description</label>
+        <div>
+            <textarea
+                type="text"
+                name="description"
+                value={values.description}
+                onChange={(e) => setValue('description', e.target.value)}
+                required
+            />
+        </div>
+        <br />
+        <button type="submit">{submitText}</button>
+      </form>
+    )
+}
