2fd744143a881e32efa4dfd1b29d9b2325910020
Felix Lopez <felix.lopez@vacasa.com>
2020-09-21 15:40:34 -0700

    Adds instance of TodoForm.

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,8 +1,11 @@
 import React, {Fragment} from 'react'
+import TodoForm from './TodoForm';
 import useTodos from '../hooks/useTodos';
+import useCreateTodos from '../hooks/useCreateTodo';
 
 export default function Posts() {
     const { isLoading, isError, data: todos, error, isFetching } = useTodos();
+    const [createTodo, { isLoading: createTodoIsLoading, error: createTodoError, success: createTodoSuccess }] = useCreateTodos()
 
     return (
         <section>
@@ -24,13 +27,31 @@ export default function Posts() {
                             {todos?.data.map((todo) => (
                                 <Fragment key={todo.id}>
                                     <input type="checkbox" name={`todo${todo.id}`} checked={todo.attributes.completed} />
-                                    <label htmlFor={`todo${todo.id}`}>{todo.attributes.text}</label>
+                                    <label htmlFor={`todo${todo.id}`}>{todo.attributes.title}</label>
                                 </Fragment>
                             ))}
                         </div>
                     </>
                 )}
             </div>
+            <hr />
+            <div>
+            <h3>Create New Todo</h3>
+            <div>
+            <TodoForm
+                onSubmit={createTodo}
+                submitText={
+                    createTodoIsLoading
+                    ? 'Saving...'
+                    : createTodoError
+                    ? 'Error!'
+                    : createTodoSuccess
+                    ? 'Saved!'
+                    : 'Create Todo'
+                }
+            />
+            </div>
+        </div>
         </section>
     )
 }
