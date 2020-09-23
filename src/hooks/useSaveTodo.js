cf1d3032de87b97988591c021bf239ace78ee5a5
Felix Oporto López <felix@MacBook-Air.domain>
2020-09-23 12:36:08 -0700

    Removes normalizer from saveTodo and refactors the code.

--- src/hooks/useSaveTodo.js
+++ src/hooks/useSaveTodo.js
@@ -4,31 +4,43 @@ import axios from 'axios'
 
 export default function useSaveTodo() {
   return useMutation(
-    (values) => 
-      axios.patch(`/api/todos/${values.data.id}`, values).then((res) => res.data),
+    todo => 
+    axios.patch(`/api/todos/${todo.id}`, {
+      data: {
+        type: 'todos', 
+        id: todo.id, 
+        attributes: {
+          ...todo
+      }
+    }
+  }).then((res) => res.data),
     {
-      onMutate: (values) => {
+      onMutate: todo => {
         // Cancel any outgoing refetches (so they don't overwrite our optimistic update)
         queryCache.cancelQueries('todos')
 
         // Snapshot the previous value
-        const previousTodos = queryCache.getQueryData(['todos']);
+        const previousTodos = queryCache.getQueryData('todos');
 
         // Optimistically update to the new value
-        queryCache.setQueryData(['todos'], (old) =>  ({
-          ...old,
-          entities: {todos: { ...old.entities.todos, [values.data.id]: {id: values.data.id, ...values.data.attributes} }},
-        }))
+        queryCache.setQueryData('todos', oldTodos =>  {
+          const newTodos =  oldTodos.data.map((oldTodo) => {
+            if(oldTodo.id === todo.id) {
+              return {...oldTodo, attributes: {...todo}}
+            } else {
+              return oldTodo;
+            }
+          })
+          return {data: newTodos};
+        })
 
         // Return the snapshotted value
-        return () => queryCache.setQueryData(['posts'], previousTodos)
+        return () => queryCache.setQueryData('todos', previousTodos)
       },
-
-       // If the mutation fails, use the value returned from onMutate to roll back
-      onError: (error, values, rollback) => rollback(),
       
-      // Always refetch after error or success:
-      onSettled: (values) => {
+      // After success or failure, refetch the todos query
+      onSettled: (newTodo, error, variables, rollback) => {
+        if(error) rollback();
         queryCache.invalidateQueries('todos')
       },
     }
