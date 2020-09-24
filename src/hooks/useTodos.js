eb37c868e7a94bdaad71f66aa09c7034ba8d51eb
Felix Oporto López <felix@MacBook-Air.domain>
2020-09-23 19:11:50 -0700

    Removes unused import normalizer.

--- src/hooks/useTodos.js
+++ src/hooks/useTodos.js
@@ -1,6 +1,5 @@
 import { useQuery } from 'react-query'
 import axios from 'axios'
-import normalize from 'jsonapi-normalizer'
 
 export default function useTodos() {
     return useQuery('todos', () => axios.get('/api/todos').then((res) => res.data))
